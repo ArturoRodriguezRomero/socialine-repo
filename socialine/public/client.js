6400ddc78ce8ca1f103a6f4a0734cfc9889cab0e
Arturo Rodriguez <arturorodriguezzromero@gmail.com>
2017-12-25 13:14:44 +0100

    Continued user logic and started message logic

--- socialine/public/client.js
+++ socialine/public/client.js
@@ -11,25 +11,43 @@ client.configure(feathers.authentication({
 }));
 
 const usersService = client.service('users');
+const messagesService = client.service('messages');
 
 var app = new Vue({
     el: '#app',
     data: {
-        client: { name: 'Arturo', pictureUrl: 'https://randomuser.me/api/portraits/men/80.jpg', about: 'Programando' },
+        client: {about:"Haciendo un post nuevo.", lastConnection:"online", name:"Fernando", pictureUrl:"https://randomuser.me/api/portraits/men/57.jpg", _id:"Q4F6fck89d8lUv9c"},
+        clientMessages: [],
         users: [],
-        selectedUser: {name: '', pictureUrl: '', about: '', lastConnection: ''},
+        selectedUser: { name: '', pictureUrl: '', about: '', lastConnection: '' },
         messages: [],
+        messageInput: ''
     },
     methods: {
-        selectUser: function (user){
+        selectUser: function (user) {
             console.log(user);
             this.selectedUser = user;
+            this.toggleClass(document.querySelector('.sidebar'), 'hidden');
+            this.toggleClass(document.querySelector('.chat-wrapper'), 'visible');
         },
         toggleSlidingPanel: function (slidingPanelClass) {
             this.toggleClass(document.querySelector(`.${slidingPanelClass}`), 'visible');
         },
-        toggleClass: function(element, className){
-            if(element.classList.contains(className)){
+        hideChat: function () {
+            this.toggleClass(document.querySelector('.sidebar'), 'hidden');
+            this.toggleClass(document.querySelector('.chat-wrapper'), 'visible');
+        },
+        sendMessage: function () {
+            messagesService.create({
+                sender: this.client._id,
+                receiver: this.selectedUser._id,
+                text: this.messageInput,
+                timestamp: new Date()
+            })
+            this.messageInput = '';
+        },
+        toggleClass: function (element, className) {
+            if (element.classList.contains(className)) {
                 element.classList.remove(className);
             } else {
                 element.classList.add(className);
@@ -41,9 +59,27 @@ var app = new Vue({
             console.log('Load', users.data);
             this.users = users.data;
         });
-        usersService.on('created', (user) => {
+        usersService.on('created', user => {
             console.log('Realtime', user);
             this.users.push(user);
+        });
+        messagesService.find({
+            query: {
+                $or: [
+                    {sender: this.client._id},
+                    {receiver: this.client._id}
+                ],
+                $sort: {
+                    timestamp: 1
+                }
+            }
+        }).then(messages => {
+            console.log(messages);
+            this.clientMessages = messages.data;
+        });
+        messagesService.on('created', message => {
+            console.log(message);
+            this.clientMessages.push(message);
         });
     }
 });
